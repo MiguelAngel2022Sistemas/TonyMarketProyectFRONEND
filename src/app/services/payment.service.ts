e5f111259652bf7ea1f982ea353c8b1f61a4d638
MiguelAngel2022Sistemas <113484673+MiguelAngel2022Sistemas@users.noreply.github.com>
2024-09-21 23:50:37 -0500

    first commit

---
+++ src/app/services/payment.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { DataPayment } from '../common/data-payment';
+import { Observable } from 'rxjs';
+import { UrlPaypalResponse } from '../common/url-paypal-response';
+import { HeaderService } from './header.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaymentService {
+  private apiUrl:string='http://localhost:8082/api/v1/payments';
+
+  constructor(private http:HttpClient, private headerService : HeaderService) { }
+
+  getUrlPaypalPayment(dataPayment:DataPayment):Observable<UrlPaypalResponse>{
+    console.log('Header:'+this.headerService.headers.get('Content-Type'))
+    return this.http.post<UrlPaypalResponse>(this.apiUrl, dataPayment, { headers: this.headerService.headers });
+  }
+
+}
